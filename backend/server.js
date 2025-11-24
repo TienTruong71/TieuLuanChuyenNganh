0d5b1bf9653574bd3e4ae27810d3157c94cae7ad
Tiến Trương <truongthanhtien2004@gmail.com>
2025-11-24 21:41:43 +0700

    contract.controller

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,7 @@ import productRoutes from './routes/client/product.route.js'
 import profileRoutes from './routes/client/profile.route.js'
 import revenueReportRoutes from './routes/admin/revenuereport.route.js'
 import promotionRoutes from './routes/admin/promotion.route.js'
+import contractRoutes from './routes/staff/sale/contract.route.js'
 // === MOUNT ROUTES ===
 app.use('/api/admin/categories', categoryRoutes)
 app.use('/api/admin/products', productAdminRoutes)
@@ -64,6 +65,7 @@ app.use('/api/client/products', productRoutes)
 app.use('/api/client/profile', profileRoutes)
 app.use('/api/admin/revenue-reports', revenueReportRoutes)  
 app.use('/api/admin/promotions', promotionRoutes)
+app.use('/api/staff/sale/contracts', contractRoutes)  
 // Uploads
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
